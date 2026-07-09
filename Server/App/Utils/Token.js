b9d6cf415485d075e7621a743f2e161df6053412
freelanceradnan <reactorbro722@gmail.com>
2026-07-09 12:49:27 +0600

    backend issue solve

--- Server/App/Utils/Token.js
+++ Server/App/Utils/Token.js
@@ -1,9 +1,9 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
-import { JWT_EXPIRATION, JWT_SECRET, JWT_SECRET } from '../Config/Config.js'
+import { JWT_EXPIRATION, JWT_SECRET } from '../Config/Config.js'
 export const TokenEncorde=(email,user_id)=>{
 try {
-    const JWT_SECRET=JWT_SECRET
+    
     const JWT_EXPIRATION={expiresIn:JWT_EXPIRATION}
     const PAYLOAD={'email':email,'user_id':user_id}
     return jwt.sign(PAYLOAD,JWT_SECRET,JWT_EXPIRATION)
@@ -13,7 +13,6 @@ try {
 }
 export const TokenDecorde=(token)=>{
     try {
-    const JWT_SECRET=JWT_SECRET
     return jwt.verify(token,JWT_SECRET)
     } catch (error) {
         return {error:{message:error.message}}
